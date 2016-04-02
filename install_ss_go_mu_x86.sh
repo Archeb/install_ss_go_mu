0ec9b4ab49752f3c02e596874636abb2ee2223eb
Bobo Liu <admin@boboweb.net>
2016-04-03 02:13:09 +0800

    就说忘了啥么。。。

--- install_ss_go_mu_x86.sh
+++ install_ss_go_mu_x86.sh
@@ -9,6 +9,7 @@ fi
 # Pre-Set
 read -p "Yout mu address:" muaddr
 read -p "Your mu key:" mukey
+read -p "Node ID:" nodeid
 echo -e "\n\033[32m===============Installtion start================\033[0m\n"
 
 # Install envs and supervisor
